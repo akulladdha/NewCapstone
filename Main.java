9c62e2013e21e5d38f97468479f59875747b8362
akulladdha <akul.laddha@gmail.com>
2024-11-21 09:50:29 -0800

    Added executor service, futures, and callables

--- Main.java
+++ Main.java
@@ -4,9 +4,12 @@ import java.util.concurrent.*;
 
 public class Main {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        ExecutorService service = Executors.newFixedThreadPool(100);
-        List<Future<Integer>> futures = new ArrayList<>();
-        long startTime = System.nanoTime();  // Capture start time
+        int cores = Runtime.getRuntime().availableProcessors();
+        int x=100;
+        ExecutorService service = Executors.newFixedThreadPool(x);
+        List<Callable<Integer>> tasks = new ArrayList<>();
+        long startTime = System.nanoTime();
+
 
         for (int i = 0; i < 1000; i++) {
             Callable<Integer> task = new Callable<Integer>() {
@@ -19,21 +22,19 @@ public class Main {
                     return count;
                 }
             };
-            futures.add(service.submit(task));
+            tasks.add(task);
         }
+        List<Future<Integer>> results = service.invokeAll(tasks);
 
-
-        for (Future<Integer> f : futures) {
-
-            Integer result = f.get();
-            System.out.println(result);
+        int result = 0;
+        for (Future<Integer> f : results) {
+            result += f.get();
         }
-
+        System.out.println(result);
 
         service.shutdown();
-        long endTime = System.nanoTime();  // Capture end time
+        long endTime = System.nanoTime();
 
-        // Calculate elapsed time in seconds and nanoseconds
         long elapsedTime = endTime - startTime;
         System.out.println("Total execution time: " + elapsedTime / 1_000_000 + " milliseconds");
     }
